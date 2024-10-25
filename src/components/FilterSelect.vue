f0ff6d1f88e678ffeb3329b76c766394dded467b
Alexander_721 <alexander3post@gmail.com>
2024-10-25 10:52:49 +0300

    complete reactive utilits

--- src/components/FilterSelect.vue
+++ src/components/FilterSelect.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
-import { ref, watch } from 'vue'
+import { isRef, ref, watch } from 'vue'
 import { shopStore } from '../store/shopStore'
 import { Category, SortDirection } from '../types/productType'
 
 const store = shopStore()
 
+const i = 12;
 const modelSort = ref(null)
 const optionsSort: SortDirection[] = ['asc', 'desc']
+
+console.log(isRef(i))
 
 const modelCategory = ref(null)
 const optionsCategory: Category[] = [
