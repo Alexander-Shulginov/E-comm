307af0b8f17b6f8679e56182c255787242cbf856
Alexander_721 <alexander3post@gmail.com>
2024-11-09 19:34:39 +0300

    add keepalive cache

--- src/components/BooksList.vue
+++ src/components/BooksList.vue
@@ -5,13 +5,15 @@ const booksStore = useBooksStore()
 </script>
 
 <template>
-    <v-row v-if="booksStore.books">
-        <v-col
-            class="v-col-12 v-col-sm-6 v-col-md-4 v-col-lg-3 v-col-xl-2"
-            v-for="book in booksStore.books.items"
-            :key="book.id"
-        >
-            <BooksCard :book="book" />
-        </v-col>
-    </v-row>
+    <keep-alive>
+        <v-row v-if="booksStore.books">
+            <v-col
+                class="v-col-12 v-col-sm-6 v-col-md-4 v-col-lg-3 v-col-xl-2"
+                v-for="book in booksStore.books.items"
+                :key="book.id"
+            >
+                <BooksCard :book="book" />
+            </v-col>
+        </v-row>
+    </keep-alive>
 </template>
