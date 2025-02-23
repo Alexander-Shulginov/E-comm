e87405949ff2fc97921ef91d5241be87a4313143
Alexander_721 <alexander3post@gmail.com>
2025-02-23 16:57:00 +0300

    add game screenshots section

--- src/components/GameInfoGallery.vue
+++ src/components/GameInfoGallery.vue
@@ -1,13 +1,43 @@
 <script setup lang="ts">
 import { Fancybox } from '@fancyapps/ui'
+import { ref } from 'vue'
+import { useSwiper } from '@/hooks/useSwiper'
+import { Pagination } from 'swiper/modules'
 import '@fancyapps/ui/dist/fancybox/fancybox.css'
+
+const swiperSimilar = ref<HTMLElement | null>(null)
+
+useSwiper(swiperSimilar, {
+    modules: [Pagination],
+    loop: true,
+    spaceBetween: 16,
+    speed: 800,
+    breakpoints: {
+        320: {
+            slidesPerView: 1
+        },
+        450: {
+            slidesPerView: 2
+        },
+        768: {
+            slidesPerView: 3
+        },
+        1024: {
+            slidesPerView: 4
+        },
+        1220: {
+            slidesPerView: 5
+        }
+    },
+    pagination: { el: '.swiper-pagination' }
+})
 
 Fancybox.bind('[data-fancybox]', {
     Toolbar: {
         display: {
             left: [],
             middle: [],
-            right: ['thumbs', 'close']
+            right: ['close']
         }
     }
 })
@@ -20,22 +50,48 @@ defineProps<{
 
 <template>
     <div class="gameInfo__gallery">
-        <a v-for="screen in screens" data-fancybox="gallery" :href="screen.img">
-            <img
-                :key="screen.id"
-                :width="screen.width"
-                :height="screen.height"
-                :src="screen.img"
-                :alt="data?.name"
-            />
-        </a>
+        <div class="swiper" ref="swiperSimilar">
+            <div class="swiper-wrapper">
+                <a
+                    v-for="screen in screens"
+                    data-fancybox="gallery"
+                    :href="screen.img"
+                    class="swiper-slide"
+                >
+                    <img
+                        class="gameInfo__gallery-img"
+                        :key="screen.id"
+                        :width="screen.width"
+                        :height="screen.height"
+                        :src="screen.img"
+                        :alt="data?.name"
+                    />
+                </a>
+            </div>
+            <div class="swiper-pagination"></div>
+        </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
-.gameInfo__gallery {
-    display: grid;
-    grid-template-columns: repeat(4, 1fr);
-    gap: 18px;
+.gameInfo {
+    &__gallery {
+        margin-bottom: 80px;
+        @media (max-width: 768px) {
+            margin-bottom: 40px;
+        }
+    }
+
+    &__gallery-img {
+        border-radius: 6px;
+        object-fit: cover;
+    }
+    .swiper {
+        padding-bottom: 40px;
+    }
+
+    a:focus-visible {
+        outline: none;
+    }
 }
 </style>
