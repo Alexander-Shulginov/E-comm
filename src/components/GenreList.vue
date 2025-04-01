ea6f5cbf53b1af55d8362c13e5edce16003643f6
Alexander_721 <alexander3post@gmail.com>
2025-04-01 13:29:12 +0300

    fix blur issue

--- src/components/GenreList.vue
+++ src/components/GenreList.vue
@@ -1,32 +1,35 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import { Pagination } from 'swiper/modules'
 import { useSwiper } from '@/hooks/useSwiper'
 import { genreList } from '@/types/genres'
 import GenreListCard from '@/components/GenreListCard.vue'
 
 const swiperGenre = ref<HTMLElement | null>(null)
 
-const { swiperInstance } = useSwiper(swiperGenre, {
-    // loop: true,
+useSwiper(swiperGenre, {
+    modules: [Pagination],
+    loop: true,
     spaceBetween: 16,
     speed: 800,
     breakpoints: {
         320: {
-            slidesPerView: 1.1
+            slidesPerView: 1
         },
         450: {
-            slidesPerView: 2.2
+            slidesPerView: 2
         },
         768: {
-            slidesPerView: 3.2
+            slidesPerView: 3
         },
         1024: {
-            slidesPerView: 4.2
+            slidesPerView: 4
         },
-        1280: {
-            slidesPerView: 6
+        1220: {
+            slidesPerView: 5
         }
-    }
+    },
+    pagination: { el: '.swiper-pagination.genre-pagination' }
 })
 </script>
 
@@ -45,20 +48,21 @@ const { swiperInstance } = useSwiper(swiperGenre, {
                 />
             </div>
         </div>
+        <div class="swiper-pagination genre-pagination"></div>
     </section>
 </template>
 
 <style lang="scss" scoped>
 .genre {
     position: relative;
-    margin-bottom: 60px;
+    margin-bottom: 56px;
 
     @media (max-width: 768px) {
         margin-bottom: 35px;
     }
 
     &__swiper {
-        padding: 5px;
+        padding: 4px 3px 40px 3px;
     }
 }
 </style>
