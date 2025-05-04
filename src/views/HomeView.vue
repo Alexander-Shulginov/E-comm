f8ea138a95c4bbda9cb17391b16f18633807bb53
Alexander_721 <alexander3post@gmail.com>
2025-05-04 12:32:09 +0300

    test async components prod

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,26 @@
 <script setup lang="ts">
+import { defineAsyncComponent } from 'vue'
 import { useHead } from '@vueuse/head'
 
 import witcherImg from '@/assets/img/main/witcher.webp'
 
-import CatalogGames from '@/components/CatalogGames.vue'
-import PopularCategory from '@/components/PopularCategory.vue'
-import PromoGame from '@/components/PromoGame.vue'
-import GenreList from '@/components/GenreList.vue'
-import PopularGames from '@/components/PopularGames.vue'
 import MainCarousel from '@/components/MainCarousel.vue'
-import BaseScrollToTop from '@/components/base/BaseScrollToTop.vue'
-import ProjectInfo from '@/components/ProjectInfo.vue'
-import BlogSection from '@/components/BlogSection.vue'
+import GenreList from '@/components/GenreList.vue'
+
+const CatalogGames = defineAsyncComponent(() => import('@/components/CatalogGames.vue'))
+const PopularCategory = defineAsyncComponent(() => import('@/components/PopularCategory.vue'))
+const PromoGame = defineAsyncComponent(() => import('@/components/PromoGame.vue'))
+const PopularGames = defineAsyncComponent(() => import('@/components/PopularGames.vue'))
+const BaseScrollToTop = defineAsyncComponent(() => import('@/components/base/BaseScrollToTop.vue'))
+const ProjectInfo = defineAsyncComponent(() => import('@/components/ProjectInfo.vue'))
+const BlogSection = defineAsyncComponent(() => import('@/components/BlogSection.vue'))
 
 useHead({
     link: [
         {
             rel: 'preload',
             as: 'image',
-            href: witcherImg,
+            href: witcherImg
         }
         // href: '/assets/witcher-Bp2S4qUw.webp'
     ]
